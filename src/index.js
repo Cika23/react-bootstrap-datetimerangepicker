5149093376fdf5020284b68b6aad33f973bd28f3
Cika23 <icikojev@gmail.com>
2018-05-16 11:58:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import $ from 'jquery';
-import 'bootstrap-daterangepicker';
+import 'daterangepicker';
 import getOptions from './getOptions';
 
 let events = ['Show', 'Hide', 'ShowCalendar', 'HideCalendar', 'Apply', 'Cancel'];
